78259548b0eadd155b7e5e4dcb35f349f7f18e3c
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-03-17 14:32:51 +0700

    latihan data BP1 2

--- src/main/kotlin/latihan.kt
+++ src/main/kotlin/latihan.kt
@@ -1,3 +1,5 @@
+import jdk.nashorn.internal.ir.CallNode.EvalArgs
+
 //fun main () {
 //    println("Hello world")
 //    println("Jiko")
@@ -12,14 +14,21 @@
 //    println("Pekerjaan kamu adalah $pekerjaan dan alamat kamu di $alamat")
 //}
 
-fun main (args: Array<String>){
-    var namaLengkap: String = "Budianto"
-    var alamat: String
-    alamat = "Yogyakarta"
+//fun main (args: Array<String>){
+//    var namaLengkap: String = "Budianto"
+//    var alamat: String
+//    alamat = "Yogyakarta"
+//
+//    var umur = 22
+//
+//    println(namaLengkap)
+//    println(alamat)
+//    println(umur)
+//}
 
-    var umur = 22
+fun main (args: Array<String>){
+    var namaLengkap = "Budianto"
+    namaLengkap = "Ahmad Budianto"
 
     println(namaLengkap)
-    println(alamat)
-    println(umur)
 }
